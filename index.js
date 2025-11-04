ab4456c65f263700096d107e5c92465dac9949ba
Hassan Amiri <hassanamiri.ai@gmail.com>
2025-11-04 14:36:28 +0100

    Refactor difficulty level prompt and improve user input handling in easy game

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function introduction() {
   console.log(msg);
 }
 function difficultyLevel() {
-  const msg1 = " Please select the difficulty level: \n";
+  const msg1 = " Please select a difficulty level: \n";
   const msg2 =
     "1. Easy (10 chances) \n 2. Medium (5 chances) \n 3. Hard (3 chances) \n ";
   console.log(msg1, msg2);
@@ -35,22 +35,50 @@ function difficultyLevel() {
 function easyGame() {
   let chances = 10;
   let trial = 0;
+  let user;
   const gameIntro =
     " Great! You have selected the Easy difficulty level. \n Let's start the game \n";
   console.log(gameIntro);
 
-  rl.question("Enter your guess: ", (number) => {
-    const user = parseint(number);
-
-    if (user === RandomNumber) {
-      console.log(
-        ` congratulation you won, \n you guessed the correct number in ${
-          trial + 1
-        } attempts. `
-      );
-    } else if (user !== RandomNumber) {
+  async function easyGameEngine() {
+    try {
+      rl.question("Enter your guess: ", (number) => {
+        user = parseInt(number);
+        if (isNaN(user)) {
+          const msg = "Text not allowed; kindly input a digit between 1 - 100";
+          console.log(msg);
+          easyGame();
+        }
+      });
+    } catch (error) {
+      console.error(error);
     }
-  });
+  }
+  easyGameEngine();
+  if (chances == 0) {
+    const msg1 = "game over you ran out of chances, you LOSERRRRRR";
+    const msg2 = "well if you wish to play again";
+    console.log(msg1, msg2);
+    difficultyLevel();
+  }
+
+  if (user !== RandomNumber) {
+    chances--;
+    trial++;
+    if (user < RandomNumber) {
+      console.log(` Incorrect! The number is greater than ${user}.`);
+      easyGameEngine();
+    } else if (user > RandomNumber) {
+      console.log(`Incorrect! The number is less than ${user}.`);
+      easyGameEngine();
+    }
+  } else {
+    console.log(
+      ` congratulation you won, \n you guessed the correct number in ${trial} attempts. `
+    );
+    console.log("if you wish to go again: ");
+    difficultyLevel();
+  }
 }
 
 introduction();
